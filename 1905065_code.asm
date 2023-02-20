69fab881505b59bce55d36b77b2739d0f59f3ff1
Arnabbndc <1905065@ugrad.cse.buet.ac.bd>
2023-02-21 05:21:55 +0600

    test5 o doneee

--- 1905065_code.asm
+++ 1905065_code.asm
@@ -4,79 +4,175 @@
 	CR EQU 0DH
 	LF EQU 0AH
 	number DB "00000$"
-	a DW 1 DUP (0000H)
-	b DW 1 DUP (0000H)
-	c DW 1 DUP (0000H)
 .CODE
-	func_a PROC
+	f PROC
 	PUSH BP
 	MOV BP, SP
 
-	PUSH 7
+	SUB SP, 2
+	PUSH 5
 	POP AX
-	MOV a, AX
+	MOV W. [BP-2], AX
 	PUSH AX
 	POP AX
-	ADD SP,0
-	POP BP
-	RET
-	func_a ENDP
-
-	foo PROC
-	PUSH BP
-	MOV BP, SP
-
-	PUSH W. [BP+6]
-	PUSH 3
+	L0:
+	PUSH W. [BP-2]
+	PUSH 0
 	POP BX
 	POP AX
-	ADD AX, BX
+	CMP AX, BX
+	JG L2
+	PUSH 0
+	JMP L3
+	L2:
+	PUSH 1
+	L3:
+
+	POP AX
+	CMP AX,0
+	JE L1
+	PUSH W. [BP+6]
+	INC W. [BP+6]
+	POP AX
+	PUSH W. [BP-2]
+	DEC W. [BP-2]
+	POP AX
+	JMP L0
+	L1:
+
+	PUSH 3
+	PUSH W. [BP+6]
+	POP BX
+	POP AX
+	XOR DX, DX
+	IMUL BX
 	PUSH AX
+	PUSH 7
+	POP BX
+	POP AX
+	SUB AX, BX
+	PUSH AX
+	POP AX
+	MOV [BP+4], AX
+	PUSH 9
 	POP AX
 	MOV W. [BP+6], AX
 	PUSH AX
 	POP AX
-	PUSH W. [BP+6]
-	POP AX
-	MOV [BP+4], AX
-	ADD SP,0
+	ADD SP,2
 	POP BP
 	RET
-	foo ENDP
+	f ENDP
 
-	bar PROC
+	g PROC
 	PUSH BP
 	MOV BP, SP
 
-	PUSH 4
+	SUB SP, 2
+	SUB SP, 2
+	PUSH W. [BP+8]
+	PUSH 0
+	CALL f
+	POP AX
+	ADD SP,2
+	PUSH AX
 	PUSH W. [BP+8]
 	POP BX
 	POP AX
-	XOR DX, DX
-	IMUL BX
+	ADD AX, BX
 	PUSH AX
-	PUSH 2
 	PUSH W. [BP+6]
-	POP BX
-	POP AX
-	XOR DX, DX
-	IMUL BX
-	PUSH AX
 	POP BX
 	POP AX
 	ADD AX, BX
 	PUSH AX
 	POP AX
-	MOV c, AX
+	MOV W. [BP-2], AX
 	PUSH AX
 	POP AX
-	PUSH c
+	PUSH 0
+	POP AX
+	MOV W. [BP-4], AX
+	PUSH AX
+	POP AX
+	L4:
+	PUSH W. [BP-4]
+	PUSH 7
+	POP BX
+	POP AX
+	CMP AX, BX
+	JL L8
+	PUSH 0
+	JMP L9
+	L8:
+	PUSH 1
+	L9:
+
+	POP AX
+	CMP AX, 0
+	JE L7
+	JMP L5
+	L6:
+	PUSH W. [BP-4]
+	INC W. [BP-4]
+	POP AX
+	JMP L4
+	L5:
+	PUSH W. [BP-4]
+	PUSH 3
+	POP BX
+	POP AX
+	XOR DX, DX
+	IDIV BX
+	PUSH DX
+	PUSH 0
+	POP BX
+	POP AX
+	CMP AX, BX
+	JE L10
+	PUSH 0
+	JMP L11
+	L10:
+	PUSH 1
+	L11:
+
+	POP AX
+	CMP AX, 0
+	JE L12
+	PUSH W. [BP-2]
+	PUSH 5
+	POP BX
+	POP AX
+	ADD AX, BX
+	PUSH AX
+	POP AX
+	MOV W. [BP-2], AX
+	PUSH AX
+	POP AX
+	JMP L13
+	L12:
+
+	PUSH W. [BP-2]
+	PUSH 1
+	POP BX
+	POP AX
+	SUB AX, BX
+	PUSH AX
+	POP AX
+	MOV W. [BP-2], AX
+	PUSH AX
+	POP AX
+	L13:
+
+	JMP L6
+	L7:
+	PUSH W. [BP-2]
 	POP AX
 	MOV [BP+4], AX
-	ADD SP,0
+	ADD SP,4
 	POP BP
 	RET
-	bar ENDP
+	g ENDP
 
 	main PROC
 	MOV AX, @DATA
@@ -87,97 +183,102 @@
 	SUB SP, 2
 	SUB SP, 2
 	SUB SP, 2
-	SUB SP, 2
-	PUSH 5
+	PUSH 1
 	POP AX
 	MOV W. [BP-2], AX
 	PUSH AX
 	POP AX
-	PUSH 6
+	PUSH 2
 	POP AX
 	MOV W. [BP-4], AX
 	PUSH AX
 	POP AX
-	PUSH 0
-	CALL func_a
-	POP AX
-	ADD SP,0
-	PUSH AX
-	POP AX
-	MOV AX, a
-	CALL print_output
 	PUSH W. [BP-2]
+	PUSH W. [BP-4]
 	PUSH 0
-	CALL foo
+	CALL g
 	POP AX
-	ADD SP,2
+	ADD SP,4
 	PUSH AX
+	POP AX
+	MOV W. [BP-2], AX
+	PUSH AX
+	POP AX
+	MOV AX, W. [BP-2]
+	CALL print_output
+	PUSH 0
 	POP AX
 	MOV W. [BP-6], AX
 	PUSH AX
 	POP AX
-	MOV AX, W. [BP-6]
-	CALL print_output
-	PUSH W. [BP-2]
-	PUSH W. [BP-4]
-	PUSH 0
-	CALL bar
-	POP AX
-	ADD SP,4
-	PUSH AX
-	POP AX
-	MOV W. [BP-8], AX
-	PUSH AX
-	POP AX
-	MOV AX, W. [BP-8]
-	CALL print_output
-	PUSH 6
-	PUSH W. [BP-2]
-	PUSH W. [BP-4]
-	PUSH 0
-	CALL bar
-	POP AX
-	ADD SP,4
-	PUSH AX
+	L15:
+	PUSH W. [BP-6]
+	PUSH 4
 	POP BX
 	POP AX
-	XOR DX, DX
-	IMUL BX
-	PUSH AX
-	PUSH 2
-	POP BX
+	CMP AX, BX
+	JL L19
+	PUSH 0
+	JMP L20
+	L19:
+	PUSH 1
+	L20:
+
 	POP AX
-	ADD AX, BX
-	PUSH AX
+	CMP AX, 0
+	JE L18
+	JMP L16
+	L17:
+	PUSH W. [BP-6]
+	INC W. [BP-6]
+	POP AX
+	JMP L15
+	L16:
 	PUSH 3
+	POP AX
+	MOV W. [BP-2], AX
+	PUSH AX
+	POP AX
+	L21:
 	PUSH W. [BP-2]
 	PUSH 0
-	CALL foo
-	POP AX
-	ADD SP,2
-	PUSH AX
 	POP BX
 	POP AX
-	XOR DX, DX
-	IMUL BX
-	PUSH AX
-	POP BX
+	CMP AX, BX
+	JG L23
+	PUSH 0
+	JMP L24
+	L23:
+	PUSH 1
+	L24:
+
 	POP AX
-	SUB AX, BX
-	PUSH AX
+	CMP AX,0
+	JE L22
+	PUSH W. [BP-4]
+	INC W. [BP-4]
 	POP AX
-	MOV W. [BP-4], AX
-	PUSH AX
+	PUSH W. [BP-2]
+	DEC W. [BP-2]
 	POP AX
+	JMP L21
+	L22:
+
+	JMP L17
+	L18:
+	MOV AX, W. [BP-2]
+	CALL print_output
 	MOV AX, W. [BP-4]
+	CALL print_output
+	MOV AX, W. [BP-6]
 	CALL print_output
 	PUSH 0
 	POP AX
 	MOV [BP+4], AX
-	ADD SP,8
+	ADD SP,6
 	POP BP
 
-	L0:
+	L14:
 
 	MOV AH, 4CH
 	INT 21H
